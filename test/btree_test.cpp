7266804f8d281ed86a4ccc53109382120e8f34e2
UncP <770778010@qq.com>
2016-10-16 21:04:15 +0800

    蘑菇

--- test/btree_test.cpp
+++ test/btree_test.cpp
@@ -1,7 +1,7 @@
 /**
  *    > Author:   UncP
  *    > Mail:     770778010@qq.com
- *    > Github:   https://www.github.com/UncP
+ *    > Github:   https://www.github.com/UncP/Mushroom
  *    > Description:
  *
  *    > Created Time: 2016-10-09 16:34:08
--- test/db_test.cpp
+++ test/db_test.cpp
@@ -1,7 +1,7 @@
 /**
  *    > Author:   UncP
  *    > Mail:     770778010@qq.com
- *    > Github:   https://www.github.com/UncP
+ *    > Github:   https://www.github.com/UncP/Mushroom
  *    > Description:
  *
  *    > Created Time: 2016-10-14 13:59:25
@@ -10,7 +10,6 @@
 #include <iostream>
 #include <fstream>
 #include <cassert>
-#include <iomanip>
 #include <string>
 
 #include "../src/iterator.hpp"
@@ -20,51 +19,53 @@ int main(int argc, char **argv)
 {
 	using namespace Mushroom;
 
-	std::ifstream in("../data/Str.txt");
-	if (!in.is_open()) {
-		std::cerr << "文件打开失败 :(\n";
-		exit(-1);
-	}
+	const char *file = "../data/Str.txt";
+	const int key_len = 10;
+	const int total = (argc == 2) ? atoi(argv[1]) : 1000000;
+
+	MushroomDB db;
+	assert(db.Open("mushroom", key_len));
+
+	char buf[BTreePage::PageByte + key_len] = {0};
+	KeySlice *key = (KeySlice *)buf;
 
 	KeySlice::SetStringForm([](const KeySlice *key) {
 		return std::string(key->Data()) + "    ";
 	});
 
-	MushroomDB db;
-	int key_len = 10;
-	db.Open("mushroom", key_len);
-
-	char buf[BTreePage::PageByte + key_len] = {0};
-	KeySlice *key = (KeySlice *)buf;
+	std::ifstream in;
+	in.open(file);
+	assert(in.is_open());
 	std::string val;
-
-	int total = argc == 2 ? atoi(argv[1]) : 100;
 	for (int i = 0; !in.eof() && i < total; ++i) {
 		in >> val;
 		memcpy(key->Data(), val.c_str(), key_len);
 		db.Put(key);
 	}
-	in.close();
-	db.Btree()->Traverse(0);
-	return 0;
-
-	memset(key, 0, sizeof(buf));
-	Iterator it(db.Btree());
-	it.Begin();
-	int count = 0;
-	for (; it.Next();) {
-		++count;
-		// if (memcmp(it.Key()->Data(), "9ohb798j6", 10) == 0)
-			// std::cout << key << it.Key() << std::endl;
-		// std::cout << key << it.Key() << std::endl;
-		// if (Compare(key, it.Key(), key_len) >= 0) {
-		// 	std::cout << key << it.Key() << std::endl;
-		// 	exit(-1);
-		// }
-		assert(Compare(key, it.Key(), key_len) < 0);
-		memcpy(key, it.Key(), sizeof(buf));
+	in.seekg(0);
+	for (int i = 0; !in.eof() && i != total; ++i) {
+		in >> val;
+		memcpy(key->Data(), val.c_str(), key_len);
+		assert(db.Get(key));
 	}
-	std::cout << "\ntotal: " << count << std::endl;
+	in.close();
+
+	int count = 0, level = 0;
+	for (;; ++level) {
+		Iterator it(db.Btree(), level);
+		if (it.Begin()) {
+			count = 0;
+			memset(key, 0, sizeof(buf));
+			for (; it.Next();) {
+				++count;
+				assert(Compare(key, it.Key(), key_len) < 0);
+				memcpy(key, it.Key(), sizeof(buf));
+			}
+		} else {
+			break;
+		}
+		std::cout << "\nlevel: " << level << "  total: " << count << std::endl;
+	}
 	db.Close();
 	return 0;
 }
